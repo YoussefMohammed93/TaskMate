7242d496db2bb24eec77048ad94aef15ce2de832
YoussefMohammed93 <youssefmohammed1590@gmail.com>
2025-03-20 07:22:43 +0200

    Dashboard UI Added

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,5 @@
-import { AppSidebar } from "@/components/app-sidebar";
 import { Header } from "@/components/landing/header";
+import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -8,12 +8,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="w-full flex min-h-screen">
         <AppSidebar />
         <main className="w-full flex-1 flex flex-col min-h-screen">
-          <Header />
+          <Header className="max-w-full bg-sidebar dark:bg-secondary" />
           <div className="flex-1 relative">
             <div className="absolute top-2 left-2 z-40">
               <SidebarTrigger />
             </div>
-            <div className="h-full p-6 pt-12">{children}</div>
+            <div className="h-full p-5">{children}</div>
           </div>
         </main>
       </div>
--- components/landing/header.tsx
+++ components/landing/header.tsx
@@ -1,10 +1,20 @@
 import { Logo } from "../logo";
+import { cn } from "@/lib/utils";
 import UserButton from "../user-button";
 
-export const Header = () => {
+interface HeaderProps {
+  className?: string;
+}
+
+export const Header = ({ className }: HeaderProps) => {
   return (
     <header className="sticky top-0 z-50 bg-card dark:bg-secondary border-b">
-      <div className="max-w-[1280px] mx-auto flex items-center justify-between gap-5 px-5 py-1.5">
+      <div
+        className={cn(
+          "max-w-[1280px] mx-auto flex items-center justify-between gap-5 px-5 py-1.5",
+          className
+        )}
+      >
         <Logo />
         <UserButton />
       </div>
