9a07064f264c916222dcc06d6bde3eb2157c0708
YoussefMohammed93 <youssefmohammed1590@gmail.com>
2025-03-20 02:17:28 +0200

    Convex Database Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { ConvexClientProvider } from "./convex-client-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ConvexClientProvider>{children}</ConvexClientProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
 export default function Main() {
-  return <main>Main Page</main>;
+  const tasks = useQuery(api.tasks.get);
+
+  return (
+    <main className="w-full h-screen flex items-center justify-center">
+      <div>
+        {tasks?.map((task) => (
+          <div
+            key={task._id}
+            className={`text-center text-5xl p-5 ${task.isCompleted ? "text-green-500" : "text-red-500"}`}
+          >
+            {task.text}
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
